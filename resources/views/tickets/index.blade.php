cde073416bc6cf43bbddc8fe07f310a75bb14404
annya <ali.annya@gmail.com>
2023-03-13 11:40:35 +0200

    Create template skeleton for windmill-dashboard

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -5,22 +5,28 @@
 @endphp
 
 <x-layout>
-    <table>
+    <table class="w-full whitespace-no-wrap">
         <thead>
-        <th>
-            Title
-        </th>
-        <th>
-           Updated
-        </th>
+        <tr
+            class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+        >
+            <th class="px-4 py-3">Title</th>
+            <th class="px-4 py-3">Updated</th>
+        </tr>
         </thead>
-        <tbody>
-            @foreach($tickets as $ticket)
-                <tr>
-                    <td>{{ $ticket->title }}</td>
-                    <td> {{ $ticket->updated_at->diffForHumans() }}</td>
-                </tr>
-            @endforeach
+        <tbody
+            class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+        >
+        @foreach($tickets as $ticket)
+            <tr class="text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                    {{ $ticket->title }}
+                </td>
+                <td>
+                    {{ $ticket->updated_at->diffForHumans() }}
+                </td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
 
